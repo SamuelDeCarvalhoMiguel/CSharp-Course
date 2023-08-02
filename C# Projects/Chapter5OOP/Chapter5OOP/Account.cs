843b5a81e9aea3792a94fa215db985769acc7416
Samuel de Carvalho Miguel <samuelmiguel.1412@gmail.com>
2023-08-02 16:21:15 -0300

    Resolução do exercício de fixação de Classe do Capítulo 05.

---
+++ C# Projects/Chapter5OOP/Chapter5OOP/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5OOP
+{
+    internal class Account
+    {
+
+        public int AccountNumber { get; private set; }
+        public string Name { get; set; }
+        public decimal Balance { get; private set; }
+
+        public Account(int AccountNumber, string name, decimal deposit)
+        {
+
+            this.AccountNumber = AccountNumber;
+            Name = name;
+            Balance = deposit;
+
+        }
+
+        public void Deposit(decimal deposit)
+        {
+            Balance += deposit;
+        }
+
+        public void Withdraw(decimal withdraw)
+        {
+            Balance -= withdraw + 5;
+        }
+
+    }
+}
---
+++ C# Projects/Chapter5OOP/Chapter5OOP/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace Chapter5OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal deposit = 0;
+
+            Console.Write("Insert account number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Insert account's owner name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Initial deposit (s/n)? ");
+            char depositChoice = char.Parse(Console.ReadLine());
+
+            if (depositChoice == 's' || depositChoice == 'S')
+            {
+
+                Console.Write("Insert the initial deposit: $");
+                deposit = decimal.Parse(Console.ReadLine());
+
+            }
+
+            Account account = new Account(accountNumber, name, deposit);
+
+            Console.WriteLine("Account data: ");
+            Console.WriteLine($"Account {account.AccountNumber}, Owner: {account.Name}, Balance: ${account.Balance:f2}\n");
+
+            Console.Write("Insert a value to deposit: $");
+            account.Deposit(decimal.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Account data: ");
+            Console.WriteLine($"Account {account.AccountNumber}, Owner: {account.Name}, Balance: ${account.Balance:f2}\n");
+
+            Console.Write("Insert a value to withdraw: $");
+            account.Withdraw(decimal.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Account data: ");
+            Console.WriteLine($"Account {account.AccountNumber}, Owner: {account.Name}, Balance: ${account.Balance:f2}\n");
+
+        }
+    }
+}
