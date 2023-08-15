cfd5d72fbe2e98f1b4d7199b2f762ad7a28bb200
Samuel de Carvalho Miguel <samuelmiguel.1412@gmail.com>
2023-08-15 17:35:59 -0300

    Mais exemplos do mesmo conteúdo do dia.

--- C# Projects/Inheritance/Inheritance/Entities/Examples/SavingsAccount.cs
+++ C# Projects/Inheritance/Inheritance/Entities/Examples/SavingsAccount.cs
@@ -18,7 +18,7 @@
             Balance += Balance * InterestRate;
         }
 
-        public override void Withdraw(double amount)
+        public sealed override void Withdraw(double amount)
         {
             base.Withdraw(amount);
             Balance -= 2.0;
