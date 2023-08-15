9494ac89ae094ba77fd9a7213ef365291ad1d680
Samuel de Carvalho Miguel <samuelmiguel.1412@gmail.com>
2023-08-14 21:45:23 -0300

    Exemplos de herança.

---
+++ C# Projects/Inheritance/Inheritance/Entities/Examples/SavingsAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace Inheritance.Entities.Examples
+{
+    internal class SavingsAccount : Account
+    {
+
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double InteresRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = InteresRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
