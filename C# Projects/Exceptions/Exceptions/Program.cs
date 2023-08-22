4c013e85642489c2077231ca996315d61d9968b7
Samuel de Carvalho Miguel <samuelmiguel.1412@gmail.com>
2023-08-21 23:44:51 -0300

    Resolução dos exercícios de Exceções.

---
+++ C# Projects/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+using System.Security.Principal;
+using Exceptions.Entities;
+using Exceptions.Entities.Exceptions;
+
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //example1();
+            exercise1();
+
+        }
+
+        static void example1()
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int resultado = n1 / n2;
+
+                Console.WriteLine(resultado);
+            }
+            catch (DivideByZeroException e )
+            {
+
+                Console.WriteLine("Error! " + e.Message);
+
+            }
+        }
+
+        static void exercise1()
+        {
+
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            String holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                acc.Withdraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
+        }
+    }
+}
