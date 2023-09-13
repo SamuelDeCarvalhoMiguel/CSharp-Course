4720d9151f96bddd0f1bd4cf00c98c7174e763cb
Samuel de Carvalho <samuelmiguel.1412@gmail.com>
2023-09-13 19:42:51 -0300

    fazendo exemplos das classes FileInfo e File.

---
+++ C# Projects/Files/Files/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      example1();
+    }
+
+    static void example1()
+    {
+      string sourcePath = @"C:\Users\Sam\Documents\CSharp-Course\C# Projects\Files\Doc.txt";
+      string targetPath = @"C:\Users\Sam\Documents\CSharp-Course\C# Projects\Files\Doc2.txt";
+
+      try
+      {
+        FileInfo fileInfo = new FileInfo(sourcePath);
+        //fileInfo.CopyTo(targetPath);  Cria uma copia do arquivo no destino apresentado.
+
+        string[] lines = File.ReadAllLines(sourcePath);
+
+        foreach (string line in lines)
+        {
+          Console.WriteLine(line);
+        }
+      }
+      catch (IOException fileException)
+      {
+        Console.WriteLine("An error occurred!");
+        Console.WriteLine(fileException.Message);
+      }
+    }
+  }
+}
+
