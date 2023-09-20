f9c2519297213fac0656b413eef8bb1290d0925e
Ailton Junior <fo104991@inter.co>
2023-09-20 18:29:37 -0300

    Exemplos de Directory e Path. Realizando exercicios de fixação.

--- C# Projects/Files/Files/Program.cs
+++ C# Projects/Files/Files/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Files
 {
@@ -11,7 +13,10 @@ namespace Files
       //example1();
       //example2();
       //example3();
-      example4();
+      //example4();
+      //example5();
+      //example6();
+      exercise();
     }
 
     static void example1()
@@ -115,6 +120,91 @@ namespace Files
         Console.WriteLine("An error occurred!");
         Console.WriteLine(fileException.Message);
       }
+    }
+
+    static void example5()
+    {
+      string path = @"C:\Users\Sam\Documents\CSharp-Course\C# Projects\Files\myfolder";
+
+      try
+      {
+        IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+
+        Console.WriteLine("FOLDERS: ");
+        foreach (string folder in folders)
+        {
+          Console.WriteLine(folder);
+        }
+
+        IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+
+        Console.WriteLine("FILES: ");
+        foreach (string file in files)
+        {
+          Console.WriteLine(file);
+        }
+
+        Directory.CreateDirectory(path + @"/newfolder");
+      }
+      catch (IOException fileException)
+      {
+        Console.WriteLine("An error occurred!");
+        Console.WriteLine(fileException.Message);
+      }
+    }
+
+    static void example6()
+    {
+      string path = @"C:\Users\Sam\Documents\CSharp-Course\C# Projects\Files\myfolder\Docs\Doc.txt";
+
+      Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+      Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+      Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+      Console.WriteLine("GetRandomFileName: " + Path.GetFileName(path));
+      Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+      Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+      Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+      Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+
+    }
+
+    static void exercise()
+    {
+      Console.Write("Enter file full path: ");
+      string sourceFilePath = Console.ReadLine();
+
+      try
+      {
+        string[] lines = File.ReadAllLines(sourceFilePath);
+
+        string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+        string targetFolderPath = sourceFolderPath + @"\out";
+        string targetFilePath = targetFolderPath + @"\summary.csv";
+
+        Directory.CreateDirectory(targetFolderPath);
+
+        using (StreamWriter sw = File.AppendText(targetFilePath))
+        {
+          foreach (string line in lines)
+          {
+
+            string[] fields = line.Split(',');
+            string name = fields[0];
+            double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+            int quantity = int.Parse(fields[2]);
+
+            Product prod = new Product(name, price, quantity);
+
+            sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+          }
+        }
+      }
+      catch (IOException e)
+      {
+        Console.WriteLine("An error occurred");
+        Console.WriteLine(e.Message);
+      }
+
     }
   }
 }
