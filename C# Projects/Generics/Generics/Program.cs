f756832e65f82a865110d1705f8a21c1ad867f6a
Samuel de Carvalho <samuelmiguel.1412@gmail.com>
2023-10-02 20:18:46 -0300

    Implementando exemplos de Generic.

---
+++ C# Projects/Generics/Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using Generics.Services;
+
+namespace Generics
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      example1();
+    }
+
+    static void example1()
+    {
+      PrintService printService = new PrintService();
+
+      Console.Write("How many values? ");
+      int valuesAmount = int.Parse(Console.ReadLine());
+
+      for (int i = 0; i < valuesAmount; i++)
+      {
+        int values = int .Parse(Console.ReadLine());
+        printService.AddValue(values);
+      }
+
+      printService.Print();
+    }
+  }
+}
---
+++ C# Projects/Generics/Generics/Services/PrintService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Services
+{
+  class PrintService
+  {
+    private int[] ValuesList = new int[10];
+    private int Count = 0;
+
+    public PrintService() { }
+
+    public void AddValue(int value)
+    {
+      if (Count == 10)
+      {
+        throw new Exception("PrintService is full!");
+      }
+      ValuesList[Count] = value;
+      Count++;
+    }
+
+    public int FirstValue()
+    {
+      if (Count == 0)
+      {
+        throw new Exception("PrintService is empty!");
+      }
+      return ValuesList[0];
+    }
+
+    public void Print()
+    {
+      Console.Write("[");
+
+      for (int i = 0; i < Count - 1; i++)
+      {
+        Console.Write($"{ValuesList[i]}, ");
+      }
+
+      if (Count > 0)
+      {
+        Console.Write($"{ValuesList[Count - 1]}");
+      }
+
+      Console.WriteLine("]");
+
+
+      if (Count > 0)
+      {
+        Console.Write($"First: {ValuesList[0]}");
+      }
+    }
+  }
+}
